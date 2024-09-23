7b1d16c520a4be2bd0af0ad94a519b076d0a438e
Mahmudul Hasan Arif <arifmahmudulhasanlu84@gmail.com>
2024-09-24 02:14:30 +0600

    new file detected

---
+++ JavaScript/logicalnot.js
@@ -0,0 +1,5 @@
+console.log(!!false);// will do true first then will convert it to false
+
+console.log(!!0); // will do false first then will convert it to false
+
+console.log(!!1); // will do true first then will convert it to true
